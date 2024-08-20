e488a85c10c5b80cbdb51ec77e088542f63fb889
Craig <craig@oppkey.com>
2024-08-19 16:05:56 -1000

    permission granting working

--- App.tsx
+++ App.tsx
@@ -1,96 +1,68 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import React from 'react';
-import type {PropsWithChildren} from 'react';
+import  React from 'react';
+import { useState } from 'react';
 import {
+  Button,
   SafeAreaView,
   ScrollView,
   StatusBar,
   StyleSheet,
   Text,
-  useColorScheme,
   View,
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import RNPermissions, {check, PERMISSIONS, RESULTS, Permission} from 'react-native-permissions';
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
 
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
 
 function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+  const [permMessage, setPermMessage ] = useState('');
+
+  const checkPerm = (permName: Permission ) => {
+    RNPermissions.check(permName).then((status) => {
+      console.log( permName, status);
+      setPermMessage( permName + ": " + status)
+
+    })
+
+  }
+
+  const checkPhotoPerm = () => {
+    checkPerm(PERMISSIONS.IOS.PHOTO_LIBRARY);
+  }
+
+  const checkPhotoAddOnly = () => {
+    checkPerm(PERMISSIONS.IOS.PHOTO_LIBRARY_ADD_ONLY);
+  }
+
+  const requestPerm = (permName: Permission) => {
+    RNPermissions.request(permName).then((status) => {
+      console.log(status);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+  }
+
+  const requestPhotoPerm = () => {
+    requestPerm(PERMISSIONS.IOS.PHOTO_LIBRARY);
+  }
+
+  const requestPhotoAddOnly = () => {
+    requestPerm(PERMISSIONS.IOS.PHOTO_LIBRARY_ADD_ONLY);
+  }
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
+    <SafeAreaView>
+      <StatusBar/>
+      <ScrollView style={styles.sectionContainer}>
+        <Text style={styles.sectionTitle}>iOS Permissions Test</Text>
+        <Button onPress={checkPhotoPerm} title='Check Photo Permissions' />
+        <Button onPress={checkPhotoAddOnly} title='Check Photo Add Perm' />
+        <Button onPress={requestPhotoPerm} title='request photo perm' />
+        <Button onPress={requestPhotoAddOnly} title='request photo add perm' />
+        <Text>{permMessage}</Text>
+
       </ScrollView>
     </SafeAreaView>
   );
